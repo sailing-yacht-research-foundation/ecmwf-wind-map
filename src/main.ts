5e299cad6c60316e1a9c2f7f9a5f6ceec356ec38
AlvinJK <ajk.alvin@gmail.com>
2022-05-04 15:17:27 +0700

    Added public bucket, upload particle png to s3

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,14 @@ import {
   generateWindParticlePNG,
   splitUVGribs,
 } from './services/ecmwfService';
+import uploadStreamToS3 from './utils/uploadStreamToS3';
 
 (async () => {
+  const bucket = process.env.AWS_S3_BUCKET || '';
+  if (!bucket) {
+    logger.error('BUCKET NOT SET! Exiting....');
+    process.exit(1);
+  }
   logger.info('WINDMAP TILES GENERATOR');
   const today = new Date();
   const yesterday = new Date();
@@ -19,7 +25,7 @@ import {
   const donwloadResult = await downloadECMWFFile('12', yesterday);
   console.log(donwloadResult);
   for (let { file, forecastTime } of donwloadResult) {
-    const folderName = file.replace('.grib', '');
+    const folderName = file.split('operating_folder/')[1].replace('.grib', '');
     const targetFolder = path.resolve(
       __dirname,
       `../operating_folder/${folderName}`,
@@ -55,14 +61,39 @@ import {
     });
     if (vrtBuilt) {
       const pngFilePath = `${targetFolder}/particle.png`;
-      await generateWindParticlePNG(vrtFilePath, pngFilePath);
-      // TODO: Upload the particle png to s3
+      const pngGenerated = await generateWindParticlePNG(
+        vrtFilePath,
+        pngFilePath,
+      );
+      if (pngGenerated) {
+        try {
+          const yearPath = String(forecastTime.getUTCFullYear());
+          const monthPath = String(forecastTime.getUTCMonth() + 1).padStart(
+            2,
+            '0',
+          );
+          const datePath = String(forecastTime.getUTCDate()).padStart(2, '0');
+          const hourPath = String(forecastTime.getUTCHours()).padStart(2, '0');
+          const { uploadPromise, writeStream } = uploadStreamToS3(
+            bucket,
+            `${yearPath}/${monthPath}/${datePath}/${hourPath}/wind_data.png`,
+          );
+          fs.createReadStream(pngFilePath).pipe(writeStream);
+          await uploadPromise;
+        } catch (error) {
+          logger.error(
+            `Error uploading wind data png: ${
+              error instanceof Error ? error.message : '-'
+            }`,
+          );
+        }
+      }
     }
     // Delete the folder
-    // try {
-    //   fs.promises.rm(targetFolder, { recursive: true });
-    // } catch (error: any) {
-    //   logger.error(`Error while cleaning up operation: ${error.message}`);
-    // }
+    try {
+      await fs.promises.rm(targetFolder, { recursive: true });
+    } catch (error: any) {
+      logger.error(`Error while cleaning up operation: ${error.message}`);
+    }
   }
 })();
