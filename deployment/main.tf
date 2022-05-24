3e868793ea1a06c6e952ee5ea370bee277f851a7
femi <femmie009@gmail.com>
2022-05-24 18:50:04 +0300

    uncommented s3lines

--- deployment/main.tf
+++ deployment/main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  #  backend "s3" {
-  #    bucket          = "syrf-ecmwf-wind-map-terraform-state"
-  #    key             = "global/s3/terraform.tfstate"
-  #    region          = "us-east-1"
-  #    dynamodb_table  = "ecmwf-wind-map-tf-state-locking"
-  #    encrypt         = true
-  #  }
+    backend "s3" {
+      bucket          = "syrf-ecmwf-wind-map-terraform-state"
+      key             = "global/s3/terraform.tfstate"
+      region          = "us-east-1"
+      dynamodb_table  = "ecmwf-wind-map-tf-state-locking"
+      encrypt         = true
+    }
 
 }
